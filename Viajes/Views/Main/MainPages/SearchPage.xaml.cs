972cd89472649e818bde5078089ef83c637f31fe
Zero <haruka@gmail.com>
2022-09-22 02:57:11 -0500

    Cambios Finales

--- Viajes/Views/Main/MainPages/SearchPage.xaml.cs
+++ Viajes/Views/Main/MainPages/SearchPage.xaml.cs
@@ -14,13 +14,32 @@ namespace Viajes.Views.Main.MainPages
     public partial class SearchPage : ContentPage
     {
         ServicesRepository _sr;
+        Users _u;
         public SearchPage(Task<List<Services>> sr,Users u)
         {
             InitializeComponent();
             _sr=new ServicesRepository();
             BindingContext = _sr;
-
+            _u = u;
             new Action(async () => ls.ItemsSource = await sr).Invoke();
+        }
+
+        private async void ls_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            ls.IsEnabled = false;
+            Device.StartTimer(TimeSpan.FromSeconds(2), () =>
+            {
+
+                ls.IsEnabled = true;
+                return false;
+            });
+            var dataitem = (Services)e.Item;
+            bool answer = await DisplayAlert("Informacion", "Desea ver mas informacion", "Yes", "No");
+
+            if (answer == true)
+            {
+                await Navigation.PushAsync(new ViewPage(dataitem, _u));
+            }
         }
     }
 }
